be705bdb53e639be1d70f638f9bf85537c8fe58a
SeedBoot <oliboots@gmail.com>
2017-08-30 18:07:49 +0100

    Add files via upload

---
+++ seedboot.js
@@ -0,0 +1,13 @@
+var getId = function(x) {
+      return document.getElementById(x);
+};
+
+var getClass = function(x) {
+  return document.getElementsByClassName(x);
+};
+
+var getSelector = function(x) {
+  return document.querySelector(x);
+};
+
+
